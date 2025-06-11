26bec151100908dabf2223ad8ff83e8022a6b290
Mike North <michael.l.north@gmail.com>
2025-06-11 09:20:17 -0500

    entangle types with server codebase

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,12 +2,13 @@ import * as yaml from "yaml";
 import * as fs from "fs";
 import path from "path";
 import { initApp } from "./app";
+import { SeedPacketModel } from "../models/seed-packet.model";
 
 const data = fs.readFileSync(
   path.join(process.cwd(), "src", "data", "seeds.yml"),
   "utf8"
 );
-const seeds = yaml.parse(data);
+const seeds: { seedPackets: SeedPacketModel[] } = yaml.parse(data);
 
 const PORT = process.env.PORT || 3000;
 
